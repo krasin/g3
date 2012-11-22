b93dea15711d6a04d0096e02389e53bbb5c3fa51
Ivan Krasin <imkrasin@gmail.com>
2012-11-22 07:15:30 +0000

    Add Node.Sub tests

--- node_test.go
+++ node_test.go
@@ -4,13 +4,13 @@ import (
 	"testing"
 )
 
-type nodeAddTest struct {
+type nodeBinaryOpTest struct {
 	node1 Node
 	node2 Node
 	want  Node
 }
 
-var nodeAddTests = []nodeAddTest{
+var nodeAddTests = []nodeBinaryOpTest{
 	{
 		Node{1, 2, 3},
 		Node{4, 5, 6},
@@ -21,6 +21,23 @@ var nodeAddTests = []nodeAddTest{
 func TestAdd(t *testing.T) {
 	for testInd, tt := range nodeAddTests {
 		got := tt.node1.Add(tt.node2)
+		if !got.Equals(tt.want) {
+			t.Errorf("Test #%d: want: %v, got %v", testInd, tt.want, got)
+		}
+	}
+}
+
+var nodeSubTests = []nodeBinaryOpTest{
+	{
+		Node{1, 2, 3},
+		Node{4, 5, 6},
+		Node{-3, -3, -3},
+	},
+}
+
+func TestSub(t *testing.T) {
+	for testInd, tt := range nodeSubTests {
+		got := tt.node1.Sub(tt.node2)
 		if !got.Equals(tt.want) {
 			t.Errorf("Test #%d: want: %v, got %v", testInd, tt.want, got)
 		}
